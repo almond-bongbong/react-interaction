168b8f0c22de74dae318733c59131518894af1a9
cmlee <bal.dongdongdong@gmail.com>
2020-01-01 18:45:17 +0900

    update alert

--- src/components/Alert/Alert.tsx
+++ src/components/Alert/Alert.tsx
@@ -1,54 +1,33 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import styles from './Alert.style.css';
+import { addRootElement } from '../../lib/generateElement';
 
 const alertContainer = document.getElementById('alert_container');
-if (!alertContainer) addRootElement(createElement('alert_container'));
+if (!alertContainer) addRootElement('alert_container');
 
-const AlertStyle = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 10000;
-  background-color: rgba(0,0,0,0.4);
+const alert = (message: string) =>
+  new Promise(resolve => {
+    const container = document.getElementById('alert_container');
 
-  & .content {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 250px;
-    padding: 20px;
-    border-radius: 6px;
-    background-color: #fff;
-    box-shadow: 1px 1px 10px 1px rgba(0,0,0,0.2);
-    text-align: center;
-    transform: translate(-50%, -50%);
-  }
-  & .message {
-    margin-bottom: 25px;
-    color: #666;
-    font-weight: 700;
-    font-size: 16px;
-  }
-`;
+    const handleClose = () => {
+      if (container) {
+        ReactDOM.unmountComponentAtNode(container);
+        resolve();
+      }
+    };
 
-const alert = message => new Promise((resolve) => {
-  const container = document.getElementById('alert_container');
-  const handleClose = () => {
-    ReactDOM.unmountComponentAtNode(container);
-    resolve();
-  };
-
-  ReactDOM.render(
-    <AlertStyle>
-      <div className="content">
-    <div className="message">
-      {newlineText(message)}
-      </div>
-      <Button inline={false} onClick={handleClose} text="확인" />
-    </div>
-    </AlertStyle>,
-  container,
-);
-});
+    ReactDOM.render(
+      <div className={styles.alert}>
+        <div className={styles.alert_content}>
+          <div className={styles.alert_message}>{message}</div>
+          <button type="button" onClick={handleClose}>
+            확인
+          </button>
+        </div>
+      </div>,
+      container,
+    );
+  });
 
 export default alert;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { default as toast, ToastOptions } from './components/Toast';
+export { default as alert } from './components/Alert';
