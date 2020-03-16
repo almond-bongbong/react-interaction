8c973c90b9117efc3750edd017884945f02d55e6
cmlee <bal.dongdongdong@gmail.com>
2020-03-16 21:15:41 +0900

    update readme

--- example/src/App.js
+++ example/src/App.js
@@ -330,7 +330,7 @@ export default function App() {
         <h2 id="tooltip">Tooltip</h2>
         <div className="example-area">
           <h3>Basic usage</h3>
-          <p>The location of the tooltip is calculated automatically.</p>
+          <p>Position of the tooltip is calculated automatically.</p>
           <div className="playground">
             <Tooltip message="Basic tooltip message">
               <button type="button" className="example-button">
